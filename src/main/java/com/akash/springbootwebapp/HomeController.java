7ad49983f29b7cba45b848015deb963586bc29e2
Akash Jaiswal <akashjaiswal@iiitdmj.ac.in>
2020-06-28 13:32:51 +0530

    Added code to getch data from client http://localhost:9999/home?name=akash

--- src/main/java/com/akash/springbootwebapp/HomeController.java
+++ src/main/java/com/akash/springbootwebapp/HomeController.java
@@ -1,5 +1,8 @@
 package com.akash.springbootwebapp;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,9 +12,12 @@ public class HomeController {
 	
 	@RequestMapping("home")
 	//@ResponseBody
-	public String home()
+	public String home(HttpServletRequest req)
 	{
-		System.out.println("Hi");
+		HttpSession session = req.getSession();
+		String name = req.getParameter("name");
+		System.out.println("Hi" + " - " + name);
+		session.setAttribute("name", name);
 		return "home";
 	}
 }
