2bfaa90706711caf0a52cad1bbcf1ea1bec56040
Akash Jaiswal <akashjaiswal@iiitdmj.ac.in>
2020-06-28 12:29:37 +0530

    Initial Commit - Spring Boot Web App

---
+++ src/main/java/com/akash/springbootwebapp/MySpringBootWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.akash.springbootwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootWebAppApplication.class, args);
+	}
+
+}
